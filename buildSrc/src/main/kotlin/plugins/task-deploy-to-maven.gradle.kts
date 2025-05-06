86387daa9a359bdbba540db6a9648d7c0c087d99
hejieehe <904696180@qq.com>
2025-05-06 16:58:22 +0800

    feat: 增加Gitee和TSvn服务提供者 #6

--- buildSrc/src/main/kotlin/plugins/task-deploy-to-maven.gradle.kts
+++ buildSrc/src/main/kotlin/plugins/task-deploy-to-maven.gradle.kts
@@ -113,9 +113,9 @@ signing {
 
 tasks.getByName("publish") {
     onlyIf {
-        project.the<SourceSetContainer>()["main"].allSource.files.isNotEmpty()
         // 演示项目不打包
-        !project.name.contains("devops-scm-provider-gitee-simple")
+        project.the<SourceSetContainer>()["main"].allSource.files.isNotEmpty() ||
+                !project.name.contains("devops-scm-provider-gitee-simple")
     }
 }
 
